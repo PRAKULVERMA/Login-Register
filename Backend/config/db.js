01182345632f34791c22e1464b92c9129c0f97c7
PRAKULVERMA <prakulverma2121@gmail.com>
2025-07-24 18:34:34 -0700

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -13,7 +13,7 @@ const connectDB = async () => {
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
   }
-  console.log("Connecting to:", process.env.MONGO_URI);
+  console.log("Connecting to:", process.env.MONGO_URL);
 };
 
 export default connectDB;
