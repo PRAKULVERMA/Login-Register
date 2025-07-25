116348c99a62727cd4ef54f95719aa4de93ba42d
PRAKULVERMA <prakulverma2121@gmail.com>
2025-07-25 10:20:56 -0700

    Update

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -4,17 +4,16 @@ import connectDB from "./config/db.js";
 import userRouter from "./routes/userRouters.js";
 import authRouter from "./routes/loginregisterRouters.js";
 import cors from "cors";
+import serverless from "serverless-http";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-// ✅ Allow multiple frontend domains (optional)
 const allowedOrigins = [
   "https://login-register-front-ten.vercel.app",
-  "http://localhost:5173", 
+  "http://localhost:5173",
 ];
 
 app.use(
@@ -30,14 +29,12 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-
 app.use("/api/users", userRouter);
-app.use("/api", authRouter); 
+app.use("/api", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+
+export const handler = serverless(app);
