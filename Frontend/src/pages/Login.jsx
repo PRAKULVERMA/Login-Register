adc5de4cce1efa3a2ae63d1d09910a9e11c8cda4
PRAKULVERMA <prakulverma2121@gmail.com>
2025-07-25 10:57:25 -0700

    Update

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom"; // <-- Add this import
+import { Link } from "react-router-dom";
 import API from "../../axiosConfig.js";
 
 function Login() {
